a549fbcbc6d6768e4110357bd27598e5b67deb9f
adarshkr-i <krofficial.com@gmail.com>
2026-09-23 19:22:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const text = "Im a beginner passionate about programming, currently learning and working toward becoming a software developer...\n...Thanks for visiting...";
+const text = "Im a beginner passionate about programming, currently learning and working toward becoming a software developer.\n...Thanks for visiting...";
 
 let index = 0;
 
